c8f425654c6cd4121027e8acd53386334df27309
renatojobal <renatojobal@gmail.com>
2019-08-23 21:41:24 -0500

    Cree mi primer hola mundo

--- platzigram/platzigram/urls.py
+++ platzigram/platzigram/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def hello_world(request):
+    return HttpResponse('Hellow world!')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello-world/', hello_world),
 ]
+
