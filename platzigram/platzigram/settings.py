47b9ce0396492a6ae5f5994df2cb1d1c35d7e4da
renatojobal <renatojobal@gmail.com>
2019-08-26 00:39:28 -0500

    APlique todas las migraciones y borre el modelo de user de la app post

--- platzigram/platzigram/settings.py
+++ platzigram/platzigram/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     # Our apss
     'posts',
+    'users',
 ]
 
 MIDDLEWARE = [
--- platzigram/users/models.py
+++ platzigram/users/models.py
@@ -1,3 +1,23 @@
+
+# Django
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    # Modelo proxy que extendera el modelo por defecto de usuario
+
+    # Relacionamos el modelo con el proxy model
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    website = models.URLField(max_length=200, blank=True)
+    biography = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+
+    picture = models.ImageField(upload_to='users/picture', blank=True, null=True)
+
+    create = models.DateField(auto_now_add=True)
+    modified = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
