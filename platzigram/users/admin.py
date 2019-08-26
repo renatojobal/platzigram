578f9a9e38e093a8e49e19c00aa2b00fc796777b
renatojobal <renatojobal@gmail.com>
2019-08-26 11:32:07 -0500

    Registrando el perfil de Profile como una clase para poder agregarle nuevas opciones

--- platzigram/users/admin.py
+++ platzigram/users/admin.py
@@ -6,4 +6,24 @@ from users.models import Profile
 
 
 # Register your models here.
-admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    # Perfil dedl admin
+    
+
+    list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
+    list_display_links = ('pk', 'user')
+    list_editable = ('phone_number', 'website', 'picture')
+    search_fields = (
+        'user__email',
+        'user__username',
+        'user__first_name',
+        'user__lastname',
+        'phone_number'
+    )
+    list_filter = (
+        'create',
+        'modified',
+        'user__is_active',
+        'user__is_staff'
+    )
