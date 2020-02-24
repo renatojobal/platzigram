eff9261ffc657d323cee87034900bfc98249aa5e
renatojobal <renatojobal@gmail.com>
2020-02-24 13:59:31 -0500

    Hasta la clase 26

--- posts/views.py
+++ posts/views.py
@@ -4,46 +4,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-# Utilities
-from datetime import datetime
-
 # Form
 from posts.forms import PostForm
 
+# Models
+from posts.models import Post
 
-posts = [
-    {
-        'title': 'Mont Blanc',
-        'user': {
-            'name': 'Yésica Cortés',
-            'picture': 'https://picsum.photos/60/60/?image=1027'
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/800/600?image=1036',
-    },
-    {
-        'title': 'Via Láctea',
-        'user': {
-            'name': 'Christian Van der Henst',
-            'picture': 'https://picsum.photos/60/60/?image=1005'
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/800/800/?image=903',
-    },
-    {
-        'title': 'Nuevo auditorio',
-        'user': {
-            'name': 'Uriel (thespianartist)',
-            'picture': 'https://picsum.photos/60/60/?image=883'
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/500/700/?image=1076',
-    }
-]
 
 @login_required
 def list_posts(request):
-    return render(request, 'posts/feed.html', {'posts': posts})
+    # Traemos todos los post
+    queryset = Post.objects.all().order_by('-created')
+
+    return render(request=request,
+        template_name='posts/feed.html',
+        context={
+            'posts': queryset
+            }
+    )
 
 
 @login_required
