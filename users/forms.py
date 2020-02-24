35a62bafab2b094255ddc7f31adeafd9ddc86a97
renatojobal <renatojobal@gmail.com>
2020-02-24 13:26:06 -0500

    Hasta la clase 25

--- users/forms.py
+++ users/forms.py
@@ -12,5 +12,5 @@ class ProfileForm(forms.Form):
     website = forms.URLField(max_length=200, required=True)
     biography = forms.CharField(max_length=500, required=False)
     phone_number = forms.CharField(max_length=20, required=False)
-    picture = forms.ImageField()
+    picture = forms.ImageField(required=False)
 
