68ef8d5b67dadc65955dbf9c32e60ee17d23b125
renatojobal <renatojobal@gmail.com>
2020-02-28 21:49:21 -0500

    Agregada documentación

--- posts/views.py
+++ posts/views.py
@@ -26,17 +26,24 @@ class PostsFeedView(LoginRequiredMixin, ListView):
 @login_required
 def create_post(request):
     """Create new post view."""
+
     if request.method == 'POST':
+        # Si el método http es POST, entra aquí y guarda un nuevo 'posst'
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
+            # Lo manda a la url dedl feed
             return redirect('posts:feed')
 
     else:
         form = PostForm()
 
+    # Si llega acá, significa que no es método http POST,
+    # entonces se entiende que el cliente solo quiere el formulario
+    # para crear el post
     return render(
         request=request,
+         # Entonces lo manda al template para crear el post:
         template_name='posts/new.html',
         context={
             'form': form,
